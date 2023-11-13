de903a6fc6e1e21876ce2c08e7fac17d4fda69c8
Cornelia Olofsson <guscornol@student.gu.se>
2023-11-13 16:06:21 +0100

    Add files to build Client structure and provide needed dependencies for frontend. This commit does not include any other functionality than providing the needed frameworks

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+      <div class="auth-inner">
+          <b-container class="py-3">
+          <router-view />
+            <p>Welcome to dentis side</p>
+          </b-container>
+        </div>
+  </div>
+</template>
