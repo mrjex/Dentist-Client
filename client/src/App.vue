c2248ae376b01b12f4eca5573eff454e0f00cd4e
Cornelia Olofsson <guscornol@student.gu.se>
2023-11-14 13:14:23 +0100

    #1 (inactive) remove print statement from App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,6 @@
       <div class="auth-inner">
           <b-container class="py-3">
           <router-view />
-            <p>Welcome to dentis side</p>
           </b-container>
         </div>
   </div>
