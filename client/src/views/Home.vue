e8cfe5ea174522f5d5f730c438cf39d37fd32708
Mohamad Khalil <mohamadk@student.chalmers.se>
2023-11-30 15:43:40 +0100

    #3 (Ongoing) Add calendar view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,27 +1,20 @@
 <template>
   <div>
-    <!-- Form for publishing time slots -->
-    <b-form @submit.prevent="publishTimeSlot">
-      <b-form-group label="Date:" label-for="dateInput">
-        <b-form-input v-model="newAppointment.date" type="date" required id="dateInput"></b-form-input>
-      </b-form-group>
-      <b-form-group label="Start Time:" label-for="startTimeInput">
-        <b-form-input v-model="newAppointment.startTime" type="time" required id="startTimeInput"></b-form-input>
-      </b-form-group>
-      <b-form-group label="End Time:" label-for="endTimeInput">
-        <b-form-input v-model="newAppointment.endTime" type="time" required id="endTimeInput"></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Publish Time Slot</b-button>
-    </b-form>
+    <vue-cal style="height: 650px" class="vuecal--blue-theme" :disable-views="['years', 'year', 'month']" hide-weekends
+      :time-cell-height="50">
+    </vue-cal>
+
     <!-- Render all appointments -->
-    <b-list-group>
+    <!-- <b-list-group>
       <b-list-group-item v-for="(appointment, index) in appointments" :key="index">
         {{ appointment.date }} from {{ appointment.startTime }} to {{ appointment.endTime }} - {{ appointment.booked ? 'Booked' : 'Available' }}
         <div class="ml-auto">
         <b-button @click="deleteAppointment(index)" variant="danger">Delete</b-button>
         </div>
       </b-list-group-item>
-    </b-list-group>
+    </b-list-group> -->
+    <add-slot />
+    <b-button class="my-3" v-b-modal.add-slot>Add Timeslot</b-button>
     <!-- show all messages in notifications -->
     <b-alert show variant="info" dismissible fade v-show="notifications.length > 0">
       <div v-for="(notification, index) in notifications" :key="index">{{ notification }}</div>
@@ -30,14 +23,19 @@
 </template>
 
 <script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import AddSlot from '../components/AddSlot.vue'
+
 export default {
   data() {
     return {
-      newAppointment: { date: '', startTime: '', endTime: '' }, // this depends on how the DB will look like
+      // newAppointment: { date: '', startTime: '', endTime: '' }, // this depends on how the DB will look like
       appointments: [], // Appointments fetched from API will be stored here
       notifications: [] // Notifications fetched from API will be stored here
     }
   },
+  components: { VueCal, AddSlot },
   methods: {
     publishTimeSlot() {
       // Simulate API call to publish time slot
