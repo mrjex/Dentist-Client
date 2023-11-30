f226b82a0f36ac8bacc2ece359724a1125e4001d
Mohamad Khalil <mohamadk@student.chalmers.se>
2023-11-30 16:08:19 +0100

    #3 (Ongoing) Add footer component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <div class="auth-inner">
+    <div class="auth-inner content">
       <Navbar />
       <b-container class="py-3">
         <router-view />
       </b-container>
     </div>
+    <BottomNav />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import BottomNav from './components/Footer.vue'
+import './styles/main.css'
 export default {
-  components: { Navbar }
+  components: { Navbar, BottomNav }
 }
 </script>
